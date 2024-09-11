19a233ae24a1135772d37418e21fcbfacd230724
IskanderDev01 <githubturabaevi01@gmail.com>
2024-09-11 09:34:22 +0500

    rollupOptions

--- src/shared/api/rtkApi.ts
+++ src/shared/api/rtkApi.ts
@@ -16,4 +16,3 @@ export const rtkApi = createApi({
     tagTypes: ['car'],
     endpoints: () => ({}),
 });
-``
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    open: true,
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
+    plugins: [react()],
+    server: {
+        open: true,
     },
-  },
-})
+    resolve: {
+        alias: {
+            '@': '/src',
+        },
+    },
+    build: {
+        rollupOptions: {
+            external: ['@fortawesome/fontawesome-svg-core'],
+        },
+    },
+});
