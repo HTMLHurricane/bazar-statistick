82e291ab4f179569edff9bfa1a66c50ef3b643e7
IskanderDev01 <githubturabaevi01@gmail.com>
2024-09-13 16:49:39 +0500

    add download

--- src/pages/info/ui/head/InfoHead.tsx
+++ src/pages/info/ui/head/InfoHead.tsx
@@ -1,10 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useInfoActions } from '@/entities/info/model/slice/infoSlice';
+import { getDefaultDateMonth } from '@/shared/lib/defaultDate/defaultDate';
 import { FlexBox } from '@/shared/ui/box/FlexBox';
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Button, DatePicker } from 'antd';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export const InfoHead = () => {
+    const { id } = useParams();
     const navigate = useNavigate();
     const { setDateMonthInfo } = useInfoActions();
 
@@ -15,18 +19,29 @@ export const InfoHead = () => {
             setDateMonthInfo(dateString);
         }
     };
+
+    const month = getDefaultDateMonth();
+
+    const downloadFile = () => {
+        const url = `https://bazar-api.aralhub.uz/export-data/car?date=${month}&car_number=${id}`;
+        window.location.href = url;
+    };
     return (
         <FlexBox cls="justify-between">
             <Button onClick={() => navigate(-1)} type="primary">
                 Назад
             </Button>
             <FlexBox>
+                <Button onClick={downloadFile}>
+                    <FontAwesomeIcon icon={faArrowDown} />
+                    скачать
+                </Button>
                 <DatePicker
                     allowClear={false}
                     picker="month"
                     onChange={onChange}
-                    placeholder="Выберите месяц"
-                    style={{ width: 200 }}
+                    placeholder="Месяц"
+                    style={{ width: 100 }}
                 />
             </FlexBox>
         </FlexBox>
