9eca84684d2a7867a699aae07859d2ee89396b7a
IskanderDev01 <githubturabaevi01@gmail.com>
2024-09-14 16:06:51 +0500

    edit default

--- src/shared/lib/defaultDate/defaultDate.ts
+++ src/shared/lib/defaultDate/defaultDate.ts
@@ -13,10 +13,13 @@ export const getDefaultDateWeek = () => {
     const year = today.getFullYear();
 
     const getWeekNumber = (date: Date) => {
-        const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
-        const pastDaysOfYear =
-            (date.getTime() - firstDayOfYear.getTime()) / 86400000;
-        return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
+        const thursday = new Date(date.getTime());
+        thursday.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7)); // Корректируем на четверг (ISO 8601)
+        const firstThursday = new Date(thursday.getFullYear(), 0, 4);
+        const weekNumber = Math.round(
+            ((thursday.getTime() - firstThursday.getTime()) / 86400000 + firstThursday.getDay() + 1) / 7
+        );
+        return weekNumber;
     };
 
     const weekNumber = getWeekNumber(today).toString().padStart(2, '0');
